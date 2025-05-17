ddd2fb14f5589f146c587820ba47eb1caf6ee331
Tetracontane <2023336@yamagatahigashi.jp>
2025-05-17 16:12:32 +0900

    vite.config.tsのベースパスを'/classMatchSystem/'に変更し、出力先を'../dist'に設定

--- class-match-react/vite.config.ts
+++ class-match-react/vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/class-match-react/', // GitHub Pages用のベースパスに変更
+  base: '/classMatchSystem/', // baseはそのまま
+  build: {
+    outDir: '../dist', // 出力先をclass-match-react外に変更
+  },
   plugins: [react()],
-})
+});
