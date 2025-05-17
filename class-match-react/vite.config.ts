3a900d195f7b0aaa3c185a077c2d966ea957b7a9
Tetracontane <2023336@yamagatahigashi.jp>
2025-05-17 16:25:57 +0900

    index.htmlとvite.config.tsでのBASE_URLのプレースホルダーを追加し、ビルド時のパス設定を改善

--- class-match-react/vite.config.ts
+++ class-match-react/vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/classMatchSystem/', // baseはそのまま
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/classMatchSystem/', // serve時はルート、build時はgithub pages上のパス
   build: {
     outDir: '../dist', // 出力先をclass-match-react外に変更
   },
   plugins: [react()],
-});
+}));
