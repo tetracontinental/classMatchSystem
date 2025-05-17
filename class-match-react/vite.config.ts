c43222ac1741898fd7f3a70b59f3c5d0d078e8ee
Tetracontane <2023336@yamagatahigashi.jp>
2025-05-17 15:22:09 +0900

    baseプロパティを相対パスに変更

--- class-match-react/vite.config.ts
+++ class-match-react/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/classMatchSystem/',
+  base: '', // 相対パスに変更
   plugins: [react()],
   define: {
     'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
