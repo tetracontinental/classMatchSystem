ba333528f951ab583443dcbcbc743389740f4b62
Tetracontane <2023336@yamagatahigashi.jp>
2025-05-17 15:27:52 +0900

    defineプロパティからVITE_SUPABASE_URLとVITE_SUPABASE_ANON_KEYの設定を削除

--- class-match-react/vite.config.ts
+++ class-match-react/vite.config.ts
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '', // 相対パスに変更
   plugins: [react()],
-  define: {
-    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
-  },
 })
